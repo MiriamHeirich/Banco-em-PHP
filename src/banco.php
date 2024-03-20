d196deea57e5695ceef9912ddf1232e1df8f54c0
Miriam Mehret <101216878+MiriamHeirich@users.noreply.github.com>
2024-03-20 11:02:40 -0300

    Teste

--- src/banco.php
+++ src/banco.php
@@ -1,13 +1,13 @@
 <?php
 
-require_once 'C:\Users\MiriamHeirich\Desktop\AulaPHP\src\Conta.php';
+require_once 'C:\Users\MiriamHeirich\Desktop\AulaPHP\Banco-em-PHP\src\Conta.php';
 
 $primeiraConta = new Conta();
 $primeiraConta->depositar(100) ;
 $primeiraConta->defineCpfTitular('05806812979');
 
 
-
+//teste
 
 
 echo $primeiraConta->recuperarSaldo();
