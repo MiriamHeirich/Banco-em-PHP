bca0a5fc890178f95918f32239e16a479b719625
Miriam Mehret <101216878+MiriamHeirich@users.noreply.github.com>
2024-03-27 11:22:59 -0300

    Getters Funcionario

--- src/Funcionario.php
+++ src/Funcionario.php
@@ -1,5 +1,29 @@
 <?php
 class Funcionario
 {
-   
+   private string $nome;
+   private string $cpf;
+   private string $cargo;
+
+
+    public function setCargo(string $cargo): self
+    {
+        $this->cargo = $cargo;
+
+        return $this;
+    }
+
+    public function setCpf(string $cpf): self
+    {
+        $this->cpf = $cpf;
+
+        return $this;
+    }
+
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
 }
