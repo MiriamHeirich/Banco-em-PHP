06a20b2d6759c07e314c2a8e6d07240ef842dd99
Miriam Mehret <101216878+MiriamHeirich@users.noreply.github.com>
2024-03-27 11:58:33 -0300

    Update Funcionario

--- src/Funcionario.php
+++ src/Funcionario.php
@@ -26,4 +26,19 @@ class Funcionario
 
         return $this;
     }
+
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    public function getCpf(): string
+    {
+        return $this->cpf;
+    }
+
+    public function getCargo(): string
+    {
+        return $this->cargo;
+    }
 }
