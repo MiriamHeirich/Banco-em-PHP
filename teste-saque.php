c6f9061ee17f221b627aee5ba4bdb30deac6c2a8
Miriam Mehret <101216878+MiriamHeirich@users.noreply.github.com>
2024-04-18 13:01:59 -0300

    Update

--- teste-saque.php
+++ teste-saque.php
@@ -6,13 +6,13 @@ use Alura\Banco\Modelo\CPF;
 use Alura\Banco\Modelo\Endereco;
 
 
-// $conta = new Conta(
-//     new Titular(
-//         new CPF('123.456.789-10'),
-//         'Vinicius Dias', 
-//         new Endereco('Petropolis', 'bairro Teste', 'Rua lá', '37')
-//     )
-// );
- $conta= new CPF('129.789.123.77');
+$conta = new Conta(
+    new Titular(
+        new CPF('123.456.789-10'),
+        'Vinicius Dias', 
+        new Endereco('Petropolis', 'bairro Teste', 'Rua lá', '37')
+    )
+);
+
 $conta->depositar('500');
-$conta->sacar('100');   
+echo 'Oii' ;$conta->sacar('100');   
